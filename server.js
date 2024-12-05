64d01d7774f6087935d1b7c16c881e63199ab06c
abelll05 <abelmerc45@gmail.com>
2024-12-05 01:43:53 -0300

    Mensaje de commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypts');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
