21e138d844b32b40acc2dad7a0c2b76c057cbeeb
abelll05 <abelmerc45@gmail.com>
2024-12-06 19:29:12 -0300

    Mensaje de commit

--- server.js
+++ server.js
@@ -2,25 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path'); // Importante para servir archivos estáticos
+const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware para registrar solicitudes
-app.use((req, res, next) => {
-  console.log(`Solicitud recibida: ${req.method} ${req.url}`);
-  next();
-});
-
-// Middleware para analizar el cuerpo de las solicitudes en JSON
+// Middleware para solicitudes JSON
 app.use(express.json());
 
 // Configuración de CORS
 const allowedOrigins = [
-  'http://localhost:3000',
-  'https://river-plate-frontend.onrender.com',
+  'http://localhost:3000', // Desarrollo local
+  'https://river-plate-frontend.onrender.com', // Despliegue en Render del frontend
 ];
 app.use(
   cors({
@@ -41,24 +35,18 @@ mongoose
     process.exit(1);
   });
 
-// Rutas de autenticación
+// Rutas de la API
 app.use('/api', authRoutes);
 
-// Sirve los archivos estáticos del frontend (React)
+// Middleware para servir archivos estáticos del frontend
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Ruta para verificar que el servidor está funcionando
-app.get('/health', (req, res) => {
-  res.send('Servidor funcionando correctamente');
-});
-
-// Manejo de rutas del frontend (React)
-// Cualquier ruta que no sea API devuelve el index.html
+// Cualquier ruta que no sea API devuelve index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Inicia el servidor
+// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
