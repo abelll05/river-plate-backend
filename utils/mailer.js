6f30a0c887ec731ae2b4ab4d4abc50dca2f3a765
abelll05 <abelmerc45@gmail.com>
2024-12-13 03:22:31 -0300

    Mensaje de commit

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,30 +1,28 @@
-require('dotenv').config();
-
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: process.env.SMTP_SECURE === 'true', // true para 465, false para otros puertos
   auth: {
-    user: process.env.SMTP_USER, 
-    pass: process.env.SMTP_PASSWORD, 
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
   },
 });
 
-const sendMail = async (to, subject, text) => {
-  const mailOptions = {
-    from: process.env.SMTP_USER, 
-    to, 
-    subject, 
-    text, 
-  };
-
+const sendMail = async (to, subject, text, html) => {
   try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('Correo enviado con éxito:', info);
-    return info;
+    await transporter.sendMail({
+      from: `"River Plate" <${process.env.SMTP_USER}>`, // Remitente
+      to, // Destinatario
+      subject, // Asunto
+      text, // Texto plano
+      html, // Cuerpo HTML
+    });
+    console.log(`Correo enviado a ${to}`);
   } catch (error) {
     console.error('Error al enviar el correo:', error.message);
-    throw new Error('Error al enviar el correo');
+    throw new Error('No se pudo enviar el correo.');
   }
 };
 
