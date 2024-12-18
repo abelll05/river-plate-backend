7122dc12c644bfcf91d174c0cb48853a765f1302
abelll05 <abelmerc45@gmail.com>
2024-12-18 02:34:16 -0300

    Mensaje descriptivo sobre los cambios realizados

--- server.js
+++ server.js
@@ -2,23 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const compression = require('compression'); // Importa el middleware de compresión
 const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware para manejar JSON
-app.use(express.json());
+app.use(express.json()); 
 
-// Middleware para compresión HTTP
-app.use(compression());
-
-// Configuración de CORS
 const allowedOrigins = [
-  'http://localhost:3000',
-  'https://river-plate-frontend.onrender.com',
+  'http://localhost:3000', 
+  'https://river-plate-frontend.onrender.com', 
 ];
 app.use(
   cors({
@@ -27,13 +21,10 @@ app.use(
   })
 );
 
-// Conexión a MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    socketTimeoutMS: 45000, // Tiempo máximo para operaciones en el socket
-    connectTimeoutMS: 10000, // Tiempo máximo para conectar
   })
   .then(() => console.log('Conexión a MongoDB exitosa'))
   .catch((err) => {
@@ -41,18 +32,14 @@ mongoose
     process.exit(1);
   });
 
-// Rutas de autenticación
 app.use('/api', authRoutes);
 
-// Servir archivos estáticos de la carpeta 'build'
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Manejar todas las rutas no definidas (React)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
